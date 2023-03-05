480a44bfd317d716e7ad5067f942255dee2011a1
tomek7667 <38402753+tomek7667@users.noreply.github.com>
2023-03-05 11:47:34 +0000

    :building_construction: Add colors.dart file
    Add colors.dart file with Colors class that will manage static colors of the app. Add initial 5 colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class Colors {
+  // Main app colors
+  static const Color black = Color(0xff1d222a);
+  static const Color darkGray = Color(0xff222831);
+  static const Color gray = Color(0xff393e46);
+  static const Color turquoise = Color(0xff00adb5);
+  static const Color white = Color(0xffeeeeee);
+}
