304b00218d4ab39086438b84f96e0f63ad428eac
tomek7667 <38402753+tomek7667@users.noreply.github.com>
2023-03-05 11:09:26 +0000

    :sparkles: Setup.sh script
    Add `setup.sh` script that makes sure the dart analytics is turned off and the flutter and dart version are appropriate

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Disable google analytics of the code
+dart --disable-analytics
+# Ensure flutter 3.7.6 verstion and Dart 2.19.3
+VERSION=$(flutter --version)
+if [[ $VERSION == *"3.7.6"* ]] && [[ $VERSION == *"2.19.3"* ]]; then
+    echo -e "\033[0;32mFlutter version is 3.7.6 and Dart version is 2.19.3\033[0m"
+else
+    echo -e "\033[0;31mFlutter version is not 3.7.6 or Dart version is not 2.19.3\033[0m"
+    exit 1
+fi
